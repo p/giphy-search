282c7cab635b7c47498e159ac4d82bcf6333c392
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-09 00:33:10 -0500

    search ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
-  render() {
+var App = React.createClass({
+  getInitialState: function() {
+    return {query: ""};
+  },
+  
+  onChange: function(e) {
+    this.setState({query: e.target.value});
+  },
+  
+  onSubmit: function(e) {
+    alert(this.state.query);
+    e.preventDefault();
+  },
+  
+  render: function() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>Giphy Search</h1>
+        <form onSubmit={this.onSubmit}>
+          <input type='text' name='query' value={this.state.query}
+            onChange={this.onChange} />
+          <input type='submit' value='Do it' />
+        </form>
+      </div>
     );
-  }
-}
+  },
+});
+
+export default App;;
