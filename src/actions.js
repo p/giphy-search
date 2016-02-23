af18ac7a82bc03a5cd4a65531b4f4c942bb2aad1
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-22 22:45:51 -0500

    fake giphy search

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,6 @@
 import * as types from './actiontypes'
 import fetch from 'isomorphic-fetch'
+import 'promise'
 
 export function nextPage() {
   return {type: types.NEXT_PAGE}
@@ -9,14 +10,29 @@ export function prevPage() {
   return {type: types.PREV_PAGE}
 }
 
+function realGiphySearch() {
+  return fetch(`http://api.giphy.com/v1/gifs/search?q={query}&api_key={api_key}&offset={offset}`)
+    .then(response => response.json())
+    .then(json => {alert(json);dispatch(fetchGifSuccess(json))})
+}
+
+function fakeGiphySearch() {
+  let promise = new Promise((resolve, reject) => 
+    resolve()
+  )
+  return promise
+}
+
+function giphySearch() {
+  return fakeGiphySearch()
+}
+
 export function fetchGifsRequest() {
   alert('fetching')
   return dispatch => {
     dispatch({type: types.FETCH_GIFS_REQUEST})
-    return fetch(`http://api.giphy.com/v1/gifs/search?q={query}&api_key={api_key}&offset={offset}`)
-      .then(response => response.json())
-      .then(json => {alert(json);dispatch(fetchGifSuccess(json))})
-    }
+    return giphySearch()
+  }
 }
 
 export function fetchGifsSuccess() {
