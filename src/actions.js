606e613ffe019614185a0d92756aaa05e7525a03
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-23 21:57:14 -0500

    clean up more

--- src/actions.js
+++ src/actions.js
@@ -38,13 +38,11 @@ function realGiphySearch(dispatch, query, offset) {
 
 function fakeGiphySearch(dispatch, query, offset) {
   let promise = new Promise((resolve, reject) => {
-  //debugger
     resolve([{
       id: 1, images: {fixed_height_small: {url: 'http://tires.tirerack.com/thumb.php?f=//cdn.app.compendium.com/uploads/user/3830be5e-cdd1-486f-a4e4-308bac9591c9/d217dcb7-b47f-4346-b0ae-8f8c650d50b5/Image/20c033b684de91bd9a4a5b720a103383/bfg_gforce_r1_pdpcrop.jpg&s=190'}},
       }]
     )
   }).then(json => {
-      //debugger
       dispatch(fetchGifsSuccess(json, query, offset))
       }
     )
@@ -59,16 +57,7 @@ export function giphySearch(query, offset) {
   }
 }
 
-export function fetchGifsRequest() {
-  alert('fetching')
-  return dispatch => {
-    dispatch({type: types.FETCH_GIFS_REQUEST})
-    return giphySearch()
-  }
-}
-
 export function fetchGifsSuccess(results, query, offset) {
-//debugger
   return {type: types.FETCH_GIFS_SUCCESS, results: results, query: query, offset: offset}
 }
 
