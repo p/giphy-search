c2f4d6773be31e612682b0660c8d6d891ae0e20f
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-28 12:17:01 -0500

    production deployment

---
+++ webpack.config.prod.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var node_modules_dir = path.resolve(__dirname, 'node_modules');
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      // There is not need to run the loader through
+      // vendors
+      exclude: [node_modules_dir],      
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
